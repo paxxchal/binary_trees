0ad8639255cd4540ef97258b5f2ec21e9f96697a
paxxchal <paschalgabrielacj8@gmail.com>
2024-03-25 13:20:32 +0100

    pushed a task

--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 #include "binary_trees.h"
 
 /**
+ * print_num - Prints a number
+ *
+ * @n: Number to be printed
+ */
+void print_num(int n)
+{
+	printf("%d\n", n);
+}
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -14,10 +24,13 @@ int main(void)
 	root = binary_tree_node(NULL, 98);
 	root->left = binary_tree_node(root, 12);
 	root->right = binary_tree_node(root, 402);
+	root->left->left = binary_tree_node(root->left, 6);
+	root->left->right = binary_tree_node(root->left, 56);
+	root->right->left = binary_tree_node(root->right, 256);
+	root->right->right = binary_tree_node(root->right, 512);
+
 	binary_tree_print(root);
-	printf("\n");
-	binary_tree_insert_right(root->left, 54);
-	binary_tree_insert_right(root, 128);
-	binary_tree_print(root);
+	binary_tree_levelorder(root, &print_num);
+	binary_tree_delete(root);
 	return (0);
 }
