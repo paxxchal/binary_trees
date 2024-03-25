a6efd7ebd82efde9d0948045c5079bd224461e1e
paxxchal <paschalgabrielacj8@gmail.com>
2024-03-25 11:16:47 +0100

    pushed first files

---
+++ binary_trees.h
@@ -0,0 +1,28 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+#include <stdlib.h>
+#include <stddef.h>
+
+/* Basic Binary Tree */
+typedef struct binary_tree_s
+{
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
+/* Binary Search Tree */
+typedef struct binary_tree_s bst_t;
+
+/* AVL Tree */
+typedef struct binary_tree_s avl_t;
+
+/* Max Binary Heap */
+typedef struct binary_tree_s heap_t;
+
+/* Function Prototypes */
+void binary_tree_print(const binary_tree_t *);
+
+#endif /* BINARY_TREES_H */
